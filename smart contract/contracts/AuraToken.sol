8d7663787f906377cbf5de9b726bd8efc175140b
illievBobby <45176733+illievBobby@users.noreply.github.com>
2018-12-13 00:40:27 +0200

    Add files via upload
    solidity 0.5.1 version

--- smart contract/contracts/AuraToken.sol
+++ smart contract/contracts/AuraToken.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.25;
+pragma solidity ^0.5.1;
 
 contract AuraToken {
 
@@ -11,13 +11,13 @@ contract AuraToken {
     string public name;                   //fancy name: eg Simon Bucks
     uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
     string public symbol;                 //An identifier: eg SBX
-    string public version = 'H1.4';       //human 0.1 standard. Just an arbitrary versioning scheme.
+    string public version = 'H1.6';       //human 0.1 standard. Just an arbitrary versioning scheme.
 
     uint256 rateBuy;
     uint256 amount1;
     uint256 amount2;
     uint256 amount3;
-    address w_owner;
+    address payable w_owner;
     uint256 w_amount;
     ///uint256 rateSell;
 
@@ -32,7 +32,7 @@ contract AuraToken {
         name = "atlant resourse";           // Set the name for display purposes
         decimals = 8;                        // Amount of decimals for display purposes
         symbol = "AURA";                     // Set the symbol for display purposes
-        rateBuy = 90000000000;              // 9 eth per AURA
+        rateBuy = 200000000000;              // 20 eth per AURA
         ///rateSell = 404000000;
         emit TotalSupply(totalSupply);
         amount1 = 0;
@@ -53,7 +53,7 @@ contract AuraToken {
 
     /// @param _owner The address from which the balance will be retrieved
     /// @return The balance
-    function balanceOf(address _owner) public constant returns (uint256 balance) {
+    function balanceOf(address _owner) public view returns (uint256 balance) {
         return balances[_owner];
     }
 
@@ -79,7 +79,7 @@ contract AuraToken {
         require((msg.sender == owner1) || (msg.sender == owner2) || (msg.sender == owner3));
         balances[_to] += _value;
         freeSupply -= _value;
-        emit Transfer(0, _to, _value);
+        emit Transfer(address(0), _to, _value);
         return true;
     }
 
@@ -88,7 +88,7 @@ contract AuraToken {
     event TotalSupply(uint256 _value);
     event Rates(uint256 _value);
     
-    function () public payable {
+    function () external payable {
         buyAura();
     }
 
@@ -101,7 +101,7 @@ contract AuraToken {
         emit Rates(rateBuy);
     }
     
-    function printCoins(uint256 _amount) public {       // must be signed from all owners
+    function printTokens(uint256 _amount) public {       // must be signed from all owners
         require(totalSupply<=1500000000000000000000000);  // 15 000 000 000 000 000 AURA
         require(_amount>0);
         require(_amount<=1500000000000000000);          // 15 000 000 000 AURA
@@ -131,15 +131,15 @@ contract AuraToken {
     ///    msg.sender.transfer(_amount / rateSell);
     ///}
     
-    function withdraw(address owner, uint256 _amount) public {  // must be signed from 2 owners
+    function withdraw(uint256 _amount) public {  // must be signed from 2 owners
         require(_amount > 0);
         require((msg.sender == owner1) || (msg.sender == owner2) || (msg.sender == owner3));
-        if((owner != w_owner) && (_amount == w_amount)) {
+        if((msg.sender != w_owner) && (_amount == w_amount)) {
             w_amount = 0;
-            owner.transfer(_amount);
+            w_owner.transfer(_amount);
         }
         else {
-            w_owner = owner;
+            w_owner = msg.sender;
             w_amount = _amount;
         }
     }
